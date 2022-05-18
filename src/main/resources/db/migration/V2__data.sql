fe83bf529a5863ab41a8b44b4d11a121d3ae34f1
annperishko <perishko32@gmail.com>
2022-05-18 16:49:39 +0300

    added db

--- src/main/resources/db/migration/V2__data.sql
+++ src/main/resources/db/migration/V2__data.sql
@@ -1,5 +1,7 @@
 INSERT INTO `football_manager`.`team` (id, name, account, commission) VALUES ('1', 'Veres', '2000000', '5');
-INSERT INTO `football_manager`.`team` (id, name, account, commission) VALUES ('2', 'Shahtar', '2000005', '6');
+INSERT INTO `football_manager`.`team` (id, name, account, commission) VALUES ('2', 'Sobaky', '131313.6', '6');
+INSERT INTO `football_manager`.`team` (id, name, account, commission) VALUES ('3', 'Capybara', '2201099.3333', '9');
 
 INSERT INTO `football_manager`.`players` (id, name, age, experience, team_id) VALUES ('1', 'Stepan Bandera', '30', '50', '1');
 INSERT INTO `football_manager`.`players` (id, name, age, experience, team_id) VALUES ('2', 'Sergiy Zhadan', '27', '31', '2');
+INSERT INTO `football_manager`.`players` (id, name, age, experience, team_id) VALUES ('3', 'Levi Ackerman', '31', '66.5', '3');
